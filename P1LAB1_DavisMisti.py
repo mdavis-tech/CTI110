88691ed120de8b7731286c7699fb50ff2de02a06
Misti <davism4460@student.faytechcc.edu>
2026-02-13 15:16:48 -0500

    Add files via upload

---
+++ P1LAB1_DavisMisti.py
@@ -0,0 +1,15 @@
+# Your Name
+# 02/13/2026
+# P1LAB1
+# This program prompts the user to enter their first and last name
+# and then displays a welcome message including their full name.
+
+# Get user's first name
+first_name = input("Enter your first name: ")
+
+# Get user's last name
+last_name = input("Enter your last name: ")
+
+# Display welcome message
+print("\nWelcome to CTI-110!")
+print("Nice to meet you,", first_name, last_name + "!")
